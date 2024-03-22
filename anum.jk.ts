16cc37140a6c6d2fdaaf6e724326a71d5e07ff9c
Meilianum <164498186+Meilianum@users.noreply.github.com>
2024-03-22 04:29:19 -0700

    there are my 16 Assignment question

---
+++ anum.jk.ts
@@ -0,0 +1,42 @@
+ /*"Q2";
+ let personName = "Anum";
+console.log("hello " , personName , "would you like to learn some typscript today");
+"Q3";*/
+// let personName = " Anum"
+// console.log( personName. toLowerCase());
+// console.log( personName. toUpperCase());
+// console.log (personName . charAt(0).toUpperCase()+ personName .slice(1).toLowerCase());
+// console.log( "anum/ Anum/ANUM");
+//Q4;
+// console.log(" Mahmoud Darwish once said" , "Nothing is harder on the soul, than the smell of dreams , while they' re  evaporating ")
+// Q5;
+// let famousPoet = "Mahmoud Darwish"
+// let message = "you can't steel the light form one who carries galaxy in their heart"
+// console.log( famousPoet , message);
+// Q6;
+// let whiteSpace = "\n\t Anum Khan\t\n"
+// console.log(whiteSpace);
+
+//  let withoutwhiteSpace = whiteSpace.trim();
+//  console.log(withoutwhiteSpace)
+// Q7; Q8
+// console.log(4 + 4);
+// console.log( 10 - 2);
+// console.log( 2* 4);
+// console.log( 16 /2);
+// Q9
+// let favouriteNumber = 3 
+// console.log( " my favouriteNUmber is " , favouriteNumber)
+// date = 12- 3 -2024
+// Q6; Q10;
+// let whiteSpace = "\n\t Anum Khan\t\n"
+// console.log(whiteSpace);
+//  let withoutwhiteSpace = whiteSpace.trim();
+//  console.log(withoutwhiteSpace)
+// Q11;
+// let friendName = [" Anum" , "Anzila", " huzaifa", "Anus"]
+// console.log(friendName[0] );
+// console.log(friendName[1] );
+// console.log(friendName[2] );
+// console.log(friendName[3] );
+
---
+++ hello.ts/typescript/helloworld.ts
@@ -0,0 +1,35 @@
+//Q11;
+//let friendName= ["Anum", " Anzila" , "huzaifa" , "Anus"];
+// console.log(friendName [0]);
+// console.log(friendName [1]);
+// console.log(friendName [2]);
+// console.log(friendName [3]);
+//Q12;
+// let friendName= ["Anum", " Anzila" , "huzaifa" , "Anus"];
+// let message= (" Study Hard")
+// console.log( "hello" ,friendName [0] ,message);
+// console.log( "hello" ,friendName [1],message);
+// console.log("hello", friendName [2] ,message);
+// console.log("hello" ,friendName [3],  message);
+// Q13;
+// let transportation : string []= [" mercedes" , "lambergini", "bike"]
+// for(let i=0; i<transportation .length ; i++){
+    // console.log("I would like to own a" + transportation [i])
+// }
+// Q14;
+//   let guest_list: string [] = [ "Ali " ,"Noor", "warda"]
+//  for(let i=0; i<guest_list .length ; i++){
+    //  console.log("Respcted Sir/ madam" + guest_list [i] +',\n We invited you to dinner tomorrow.\n\n Thankyou')
+//  } 
+//Q15;
+// let not_come : String = "Ali ";
+// let new_guest :string = "Mohsin";
+// for(let i=0; i<guest_list .length ; i++){
+    //  console.log("Respcted Sir/ madam" + guest_list [i] +',\n We invited you to dinner tomorrow.\n\n Thankyou')
+// }
+// console.log(`Mr. ${not_come} will not coming tomorrow for dinner.`)
+// Q16;
+// guest_list.unshift (" Muneeb " , " Mark " , " Theo")
+// for(let i=0; i<guest_list .length ; i++){
+    // console.log("Respcted Sir/ madam" + guest_list [i] +',\n We invited you to dinner tomorrow.\n\n Thankyou')
+// }
